28baa6d42e7bc725c2edc697802abc65141d40cd
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-19 22:18:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = "c9683a8ec3b886c18219f650fc8ed429"
 BOT_TOKEN = "8411080834:AAE85QH-LpaiOpht-RSMpwYQPus1jHONnu4"
 SESSION_STRING = "BQE-4i0ASxu8TXk4s870tFMn-D2Ijs-7DaTep8qcmRnZuowGYTiKDzzy9fKRT3pCc7aFI9oql0Rp5k1FkymDhRbewYPN11p5G7exMCs-z2bdMPuRoJCF60r7p_xq0TBjtLw5P1f-pXHHRxeXSAq0nKyNglv2pZ-GVCbYL4J-OwIkfck4wZyfiU0H58LZla5Il4VmVww-ewK3roa4mVjIxGKYoFva7LqYEf9Iti77jLz7HW7gCfuNessLDXqH1se4DuOSmoJzbacJxofENDQJChGjP4K7gbkMQQKwjCQfndvTmHLyDnc5jDqwfngZK1ogepmyiXZhhzHVebIieznK4DXTM1Q7pAAAAAHKarFXAA"
 # --- Random image links ---" # Apni session string yahan puri daal dena
-
+OWNER_ID = 8566803656
 # --- Global Tracking ---
 queues = {}
 playing_messages = {} # {chat_id: message_id} - Timer edit karne ke liye
